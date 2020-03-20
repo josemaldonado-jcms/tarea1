5b2de51fdab259816361ea1a9489c2ba80ed50a0
josemaldonado-jcms <josmais666@gmail.com>
2020-03-20 00:29:49 -0400

    primera version

---
+++ Desktop/secion3/02 typescript/tarea1/tarea1.js
@@ -0,0 +1,8 @@
+"use strict";
+var numero = 0;
+var nombre = 'sin nombre';
+var edad = 0;
+numero = prompt("ingrese su ci");
+nombre = prompt("ingrese su nombre");
+edad = prompt('edad');
+console.log = ('Hola' + nombre + ' tienes ' + edad + "y tu CI es:" + edad);
